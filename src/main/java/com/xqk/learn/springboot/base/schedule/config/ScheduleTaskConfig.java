b0b70919d38e113a2a739a85f689b8a8ea9f5fdb
熊乾坤 <2372389705@qq.com>
2019-09-15 21:56:29 +0800

    2019.9.15晚-新增部分定时任务学习内容

---
+++ src/main/java/com/xqk/learn/springboot/base/schedule/config/ScheduleTaskConfig.java
@@ -0,0 +1,15 @@
+package com.xqk.learn.springboot.base.schedule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Aviator
+ * @date 2019-9-15
+ */
+
+@EnableScheduling
+@Configuration
+public class ScheduleTaskConfig {
+
+}
