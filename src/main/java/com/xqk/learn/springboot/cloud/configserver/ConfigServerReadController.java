b74a51e7dcf7b919e609a13ff6a679d2211d51ec
xiongqiankun <xiongqiankun@kedacom.com>
2021-04-27 18:35:41 +0800

    提交一些文件。

--- src/main/java/com/xqk/learn/springboot/cloud/configserver/ConfigServerReadController.java
+++ src/main/java/com/xqk/learn/springboot/cloud/configserver/ConfigServerReadController.java
@@ -2,6 +2,7 @@ package com.xqk.learn.springboot.cloud.configserver;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-03-27 0:59
  */
 @RestController
+@Profile("microservices")
 @RequestMapping("/config-server")
 public class ConfigServerReadController {
     @Value("${username}")
