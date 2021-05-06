07209c4538c2c6fd31d7034b02715017fb8c5eca
xiongqiankun <xiongqiankun@kedacom.com>
2021-05-06 18:41:16 +0800

    新增AOP学习demo

--- src/main/java/com/xqk/learn/springboot/aop/callself/CallSelfMethodServiceImpl.java
+++ src/main/java/com/xqk/learn/springboot/aop/callself/CallSelfMethodServiceImpl.java
@@ -2,6 +2,7 @@ package com.xqk.learn.springboot.aop.callself;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,12 +24,24 @@ public class CallSelfMethodServiceImpl implements CallSelfMethodService {
         log.info("------------------------------------");
         //此方法调用没有被AOP增强
         methodB();
+
         log.info("------------------------------------");
         //方法一：使用自我注入的对象上调用方法可以被AOP增强
         callSelfMethodService.methodB();
+
         log.info("------------------------------------");
         //方法二：从AopContext获取暴露的代理类，调用代理类的方法。需要@EnableAspectJAutoProxy注解的exposeProxy为true
         ((CallSelfMethodService) AopContext.currentProxy()).methodB();
+
+        log.info("------------------------------------");
+        //是否代理对象
+        log.info("AopUtils.isAopProxy(AopContext.currentProxy()):[{}]", AopUtils.isAopProxy(AopContext.currentProxy()));
+
+        //是否cglib 代理对象
+        log.info("AopUtils.isCglibProxy(AopContext.currentProxy():[{}]", AopUtils.isCglibProxy(AopContext.currentProxy()));
+
+        //是否jdk动态代理
+        log.info("AopUtils.isJdkDynamicProxy(AopContext.currentProxy()):[{}]", AopUtils.isJdkDynamicProxy(AopContext.currentProxy()));
     }
 
     @Override
