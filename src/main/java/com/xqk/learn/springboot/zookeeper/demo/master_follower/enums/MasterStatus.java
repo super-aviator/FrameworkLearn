3f4fb910902aa277d5d4cf49ca503e6b5eb31c3d
xiongqiankun <xiongqiankun@focusmedia.cn>
2022-03-03 16:32:12 +0800

    删除zookeeper代码，移动到JavaSE项目

--- src/main/java/com/xqk/learn/springboot/zookeeper/demo/master_follower/enums/MasterStatus.java
+++
@@ -1,5 +0,0 @@
-package com.xqk.learn.springboot.zookeeper.demo.master_follower.enums;
-
-public enum MasterStatus {
-    ELECTED, NOT_ELECTED;
-}
--- src/main/java/com/xqk/learn/springboot/zookeeper/demo/master_follower/enums/WorkerStatusEnum.java
+++
@@ -1,6 +0,0 @@
-package com.xqk.learn.springboot.zookeeper.demo.master_follower.enums;
-
-public enum WorkerStatusEnum {
-    IDLE,
-    RUNNING
-}
