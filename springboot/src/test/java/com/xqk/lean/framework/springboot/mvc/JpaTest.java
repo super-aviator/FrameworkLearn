cb4bd3390a4da9edceb8585d5f276b47cac25b76
aviator <2372389705@qq.com>
2024-12-21 15:51:07 +0800

    update compile error

--- springboot/src/test/java/com/xqk/lean/framework/springboot/mvc/JpaTest.java
+++ springboot/src/test/java/com/xqk/lean/framework/springboot/mvc/JpaTest.java
@@ -1,6 +1,6 @@
 package com.xqk.lean.framework.springboot.mvc;
 
-import com.xqk.learn.framework.springboot.data.jpa.repository.UserJpaRepository;
+import com.xqk.lean.framework.springboot.data.jpa.repository.UserJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
